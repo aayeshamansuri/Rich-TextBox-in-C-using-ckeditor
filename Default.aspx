655a703721bb71163f8bc09fcea3786872138267
aayeshamansuri <aayeshamansuri8866@gmail.com>
2020-07-28 23:34:30 +0530

    First commit

---
+++ Default.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" ValidateRequest="false" Inherits="_Default" %>
+
+<!DOCTYPE>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <script src="ckeditor/ckeditor.js"></script>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+   <asp:TextBox ID="text" runat="server" TextMode="MultiLine"></asp:TextBox>
+   <script>
+       CKEDITOR.replace("text"); 
+   </script> 
+
+
+        <br />
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" Text="save" OnClick="save"  />
+         <asp:Button ID="Button2" runat="server" Text="show" OnClick="showData"  />
+       
+        <br />
+        <br />
+        <br />
+        <br />
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+       
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class _Default : System.Web.UI.Page
+{
+    SqlConnection cn = new SqlConnection();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Label1.Text = "";
+        cn.ConnectionString = @"Data Source=.\SQLEXPRESS;AttachDbFilename=C:\Users\Mansuri\Desktop\RichTextbox\App_Data\Database.mdf;Integrated Security=True;User Instance=True";
+    }
+    protected void save(object sender, EventArgs e)
+    {
+        cn.Open();
+        SqlCommand cmd = new SqlCommand("INSERT INTO new (data) values (@data)" , cn);
+        cmd.Parameters.AddWithValue("@data",text.Text);
+        cmd.ExecuteNonQuery();
+        text.Text = "";
+        cn.Close();
+
+    }
+
+    protected void showData(object sender, EventArgs e)
+    {
+        cn.Open();
+        SqlCommand cmd = new SqlCommand("SELECT data from new where id = 14", cn);
+        Label1.Text = cmd.ExecuteScalar().ToString();
+        cn.Close();
+    }
+}
